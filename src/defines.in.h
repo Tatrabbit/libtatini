7312e9ea551dcff74517ae25e21691d5baa05965
Tatrabbit <alx1213@gmail.com>
2024-10-14 14:10:52 -0400

    Open files

--- src/defines.in.h
+++ src/defines.in.h
@@ -3,4 +3,19 @@
 
 #define VERSION_STRING "${APP_VERSION_NUMBER}"
 
+#if (${CMAKE_C_STANDARD} >= 23)
+#define NODISCARD [[nodiscard]]
+#else
+#define NODISCARD
+#endif
+
+
+#cmakedefine CMAKE_GCC_LIKE
+
+#ifdef CMAKE_GCC_LIKE
+#define WARN_UNUSED __attribute__((warn_unused_result))
+#else
+#define WARN_UNUSED
+#endif
+
 #endif //DEFINES_IN_H
--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include "defines.h"
 
+#include <assert.h>
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -16,13 +17,16 @@ enum {
 
 typedef struct {
     const char *filename;
+    FILE *handle;
     size_t size;
     char *contents;
 } file_info_t;
 
 typedef struct {
     const char *program_name;
-    file_info_t *file_infos;
+
+    size_t input_count;
+    file_info_t *input_infos;
 
     int verbose;
 } options_t;
@@ -95,38 +99,27 @@ static size_t get_fsize(FILE *f) {
     return size;
 }
 
-static int parse_file(const char *file_name, char **buffer) {
-    FILE *file = fopen(file_name, "r");
-    if (file == NULL)
-        return 1;
-    const size_t size = get_fsize(file);
+typedef int (*filename_func_t_)(const char *filename, void *data);
 
-    *buffer = malloc(size + 1);
-    if (*buffer == NULL)
-        return 1;
+#define filename_func_t WARN_UNUSED filename_func_t_
 
-    fseek(file, 0, SEEK_SET);
-    fread(buffer, size, 1, file);
-    fclose(file);
-
-    return 0;
-}
-
-typedef void (*filename_func_t)(const char *filename, void *data);
-
-static int iterate_argv_files_disambiguated(const int argc, const char *const *argv, const filename_func_t func,
-                                            void *data) {
+WARN_UNUSED
+static int iterate_argv_files_disambiguated(const int argc, const char *const *argv,
+                                            const filename_func_t func, void *data) {
     for (int i = 1; i < argc; ++i) {
         const char *arg = argv[i];
 
         if (strcmp(arg, "--") == 0)
             return print_usage_error("only one -- permitted\n");
 
-        func(arg, data);
+        int err;
+        if ((err = func(arg, data)) != 0)
+            return err;
     }
     return 0;
 }
 
+WARN_UNUSED
 static int iterate_argv_files(const int argc, const char *const *argv, const filename_func_t func, void *data) {
     for (int i = 1; i < argc; ++i) {
         const char *arg = argv[i];
@@ -136,56 +129,105 @@ static int iterate_argv_files(const int argc, const char *const *argv, const fil
                 return iterate_argv_files_disambiguated(argc, &arg, func, data);
             continue;
         }
-        func(arg, data);
+
+        int err;
+        if ((err = func(arg, data)) != 0)
+            return err;
     }
 
     return 0;
 }
 
-static void count_file_arg(const char *filename, void *data) {
+static int count_file_arg(const char *filename, void *data) {
     (void) filename;
     *(int *) data += 1;
+    return 0;
 }
 
-static void get_file_arg(const char *filename, void *data) {
-    size_t *index = data;
-    options.file_infos[*index].filename = filename;
-    *index += 1;
-}
+static int count_input_files(const int argc, const char *const *argv) {
+    int err, count = 0;
+    if ((err = iterate_argv_files(argc, argv, count_file_arg, &count)))
+        return -err;
 
-static int count_input_files(int argc, const char *const *argv) {
-    int count = 0;
-    iterate_argv_files(argc, argv, count_file_arg, &count);
     return count;
 }
 
-static int get_input_filenames(int argc, const char *const *argv) {
-    const int file_count = count_input_files(argc, argv);
+typedef struct {
+    size_t index;
+    size_t total_size;
+} file_arg_data_t;
 
-    if (file_count <= 0) {
-        print_info("No input files.");
-        return 0;
-    }
-    print_verbose("Reading info from %d files...\n", file_count);
+static int get_file_arg(const char *filename, void *data) {
+    file_arg_data_t *arg_data = data;
+    file_info_t *info = &options.input_infos[arg_data->index++];
 
-    options.file_infos = malloc(sizeof(file_info_t));
+    print_verbose("Opening file #%d (%s)\n", arg_data->index, filename);
 
-    if (options.file_infos == NULL) {
-        return print_mem_error();
-    }
+    info->handle = fopen(filename, "r");
+    if (info->handle == NULL)
+        return print_file_error("Cannot open %s", filename);
 
-    size_t file_arg_index = 0;
-    (void) iterate_argv_files(argc, argv, get_file_arg, &file_arg_index);
-
-    for (size_t i = 0; i < file_count; ++i) {
-        print_info("Opening file %s\n", options.file_infos[i].filename);
-        // TODO
-    }
+    info->size = get_fsize(info->handle) + 1;
+    arg_data->total_size += info->size;
 
     return 0;
 }
 
-static int parse_input_files() {
+static int allocate_input_files(const int argc, const char *const *argv) {
+    options.input_count = count_input_files(argc, argv);
+    if (options.input_count == 0)
+        return 0;
+
+    if (options.input_count < 0) // ERROR
+        return options.input_count;
+
+    print_verbose("Reading info from %d files...\n", options.input_count);
+
+    // Allocate and zerofill
+    options.input_infos = malloc(sizeof(file_info_t) * options.input_count);
+    if (options.input_infos == NULL) {
+        return print_mem_error();
+    }
+    memset(options.input_infos, 0, sizeof(file_info_t) * options.input_count);
+
+    return 0;
+}
+
+static int read_input_files(const int argc, const char *const *argv) {
+    int err;
+
+    // Get filename, count file arguments
+    if ((err = allocate_input_files(argc, argv)) != 0)
+        return err;
+
+    if (options.input_count == 0) {
+        print_info("No input files.");
+        return 0;
+    }
+
+    // Open, each get size
+    file_arg_data_t arg_data = {.index = 0, .total_size = 0};
+    if ((err = iterate_argv_files(argc, argv, get_file_arg, &arg_data)))
+        return err;
+
+    // Allocate all buffers
+    char *buffer = malloc(arg_data.total_size);
+    if (buffer == NULL)
+        return print_mem_error();
+
+    // Read & assign buffers
+    for (size_t i = 0; i < options.input_count; ++i) {
+        file_info_t *info = &options.input_infos[i];
+
+        fseek(info->handle, 0, SEEK_SET);
+        const size_t read_count = fread(buffer, info->size - 1, 1, info->handle);
+        if (read_count != 1)
+            return print_file_error("Error reading file #%d (%s)\n", i, info->filename);
+
+        info->contents = buffer;
+        buffer += info->size;
+    }
+
     return 0;
 }
 
@@ -196,6 +238,40 @@ static int parse_options(const int argc, const char *const *argv) {
     return 0;
 }
 
+static void close_input_files() {
+    assert(options.input_count >= 1);
+
+    for (size_t i = 0; i < options.input_count; ++i) {
+        file_info_t *info = &options.input_infos[i];
+
+        FILE *file = info->handle;
+        if (file == NULL)
+            break;
+
+        fclose(file);
+        info->handle = NULL;
+    }
+}
+
+static void cleanup_file_infos() {
+    if (options.input_infos == NULL)
+        return;
+
+    assert(options.input_count >= 1);
+
+    close_input_files();
+
+    if (options.input_infos[0].contents != NULL)
+        free(options.input_infos[0].contents);
+
+    free(options.input_infos);
+}
+
+static int cleanup(const int err) {
+    cleanup_file_infos();
+    return err;
+}
+
 int main(const int argc, char **argv) {
     memset(&options, 0, sizeof(options));
     options.verbose = 1; // TODO
@@ -203,18 +279,11 @@ int main(const int argc, char **argv) {
     show_version();
 
     int err;
-
     if ((err = parse_options(argc, (const char **) argv)))
-        goto cleanup;
+        return cleanup(err);
 
-    if ((err = get_input_filenames(argc, (const char **) argv)))
-        goto cleanup;
+    if ((err = read_input_files(argc, (const char **) argv)))
+        return cleanup(err);
 
-    err = parse_input_files();
-
-cleanup:
-    if (options.file_infos != NULL)
-        free(options.file_infos);
-
-    return err;
+    return cleanup(0);
 }
